c84553c7d687d45c05283ff4b6da75e1e584ef62
antonvh <antonhaninvl@gmail.com>
2019-10-20 18:29:05 +0300

    feat: add swiper mobile

--- src/script.js
+++ src/script.js
@@ -44,3 +44,95 @@ document.querySelector('.control.right').addEventListener('click', function () {
         nextItem(currentItem);
     }
 });
+
+const swipedetect = (el) => {
+    let surface = el;
+    let startX = 0;
+    let startY = 0;
+    let distX = 0;
+    let distY = 0;
+    let startTime = 0;
+    let elapsedTime = 0;
+
+    let theshold = 150;
+    let restraint = 100;
+    let allowedTime = 300;
+
+    surface.addEventListener('mousedown', function (e) {
+        startX = e.pageX;
+        startY = e.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    }, false);
+
+    surface.addEventListener('mouseup', function (e) {
+        distX = e.pageX;
+        distY = e.pageY;
+        elapsedTime = new Date().getTime() - startTime;
+        if (elapsedTime <= allowedTime) {
+            if (Math.abs(distX) >= theshold && Math.abs(distY) <= restraint) {
+                if ((distX > 0)) {
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else if (isEnabled) {
+                    nextItem(currentItem);
+                }
+            }
+        }
+        e.preventDefault();
+    }, false);
+    surface.addEventListener('touchstart', function (e) {
+        if (e.target.classList.contains('arrow') || e.target.classList.contains('control')) {
+            if (e.target.classList.contains('left')) {
+                if (isEnabled) {
+                    previousItem(currentItem);
+                }
+            } else if (isEnabled) {
+                nextItem(currentItem);
+            }
+        }
+        var touchobj = e.changedTouches[0];
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    }, false);
+
+    surface.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    }, false);
+
+    surface.addEventListener('touchend', function (e) {
+        var touchobj = e.changedTouches[0];
+        distX = touchobj.pageX - startX;
+        distY = touchobj.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+        if (elapsedTime <= allowedTime) {
+            if (Math.abs(distX) >= theshold && Math.abs(distY) <= restraint) {
+                if ((distX > 0)) {
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else if (isEnabled) {
+                    nextItem(currentItem);
+                }
+            }
+        }
+        e.preventDefault();
+    }, false);
+}
+var el = document.querySelector('.carusel');
+swipedetect(el);
+
+let isPress = true;
+function showEnum() {
+    let projectArray = document.querySelectorAll('.project-name');
+    if (isPress) {
+        isPress = false;
+        projectArray[currentItem].classList.add('show-num');
+    } else {
+        isPress = true
+        projectArray[currentItem].classList.remove('show-num');
+    }
+}
